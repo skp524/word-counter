22a02e9ff01fbcaaa9ccf96da2dea1417c6722ec
shashank.pandey <shashank.pandey>
2021-05-24 20:46:21 +0530

    add SameWord Count

--- src/App.js
+++ src/App.js
@@ -9,19 +9,42 @@ class App extends Component {
       vowels:0,
       consonant:0,
       semiVowels:0,
+      sameWords:[],
     }
   }
-  count=()=>{
-    
-    const {text}=this.state;
+
+  findSameWord=()=>{
+    const {text} = this.state;
     if(text!==""){
-      const words = text.match(/\S+/gi).length;
+      let tempArr = text.match(/\S+/gi);
+       let sameWords = [];
+       let count = 1;
+      for (let i = 0; i < tempArr.length; i++)
+       {
+          if (tempArr[i] === tempArr[i + 1]) 
+          { 
+            count++;
+      }
+       else {
+      if (count >1)
+      {
+        sameWords = [...sameWords, {word:tempArr[i],count:count}];
+        count = 1;
+      } 
+    }}
+      this.setState({sameWords:sameWords})
+  } 
+  }
+  count=()=>{
+    const {text}=this.state;
+    const words = text !==""?text.match(/\S+/gi).length:0;
     const vowels = text.match(/[aeiou]/gi)!==null?text.match(/[aeiou]/gi).length:0;
     const semiVowels = text.match(/[wy]/gi)!==null?text.match(/[wy]/gi).length:0;
-    const alphabet = text.match(/[a-z]/gi)!==null?text.match(/[a-z]/gi).length:0;
-    const consonant=alphabet-vowels;
+    const consonant = text.match(/[bcdfghjklmnpqrstvwxyz]/gi)!==null?text.match(/[bcdfghjklmnpqrstvwxyz]/gi).length:0;
     this.setState({vowels:vowels,semiVowels:semiVowels,consonant:consonant,words:words});
-  }}
+    this.findSameWord()
+  
+}
   render(){
     const {text}=this.state;
   return (
@@ -32,10 +55,11 @@ class App extends Component {
         onChange={(e)=>{this.setState({text:e.target.value},()=>this.count())
         }}
       />
-      <h2>vowels {this.state.vowels}</h2>
+      <h2>Vowels {this.state.vowels}</h2>
       <h2>Semi vowels {this.state.semiVowels}</h2>
-      <h2>cons {this.state.consonant}</h2>
-      <h2>words {this.state.words}</h2>
+      <h2>Consonant {this.state.consonant}</h2>
+      <h2>Words {this.state.words}</h2>
+      <h2>Same words {this.state.sameWords.map(word=>{return <h2>{word.word} {word.count}</h2>})}</h2>
     </div>
   );
 }}
